9d7f7d435ca8af9351d41b82396e4e6392e874ed
MEER ATHIF ALI I <meerathifali@gmail.com>
2021-09-07 13:51:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import PriceCard from './priceCard';
+import './App.css';
+
+function App() {
+  const pricingData = [
+    {
+      packageName: "FREE",
+      price: "$0",
+      period: "month",
+      features: [
+        {
+          name: "Single User",
+          isEnabled: true
+        },
+        {
+          name: "5GB Storage",
+          isEnabled: true
+        }, {
+          name: "Unlimited Public Projects",
+          isEnabled: true
+        }, {
+          name: "Community Access",
+          isEnabled: true
+        }, {
+          name: "Unlimited Private Projects",
+          isEnabled: false
+        }, {
+          name: "Dedicated Phone Support",
+          isEnabled: false
+        }, {
+          name: "Free Subdomain",
+          isEnabled: false
+        }, {
+          name: "Monthly Status Reports",
+          isEnabled: false
+        }
+      ]
+    },
+    {
+      packageName: "PLUS",
+      price: "$9",
+      period: "month",
+      features: [
+        {
+          name: "5 Users",
+          isEnabled: true,
+          isBold: true
+        },
+        {
+          name: "50GB Storage",
+          isEnabled: true
+        }, {
+          name: "Unlimited Public Projects",
+          isEnabled: true
+        }, {
+          name: "Community Access",
+          isEnabled: true
+        }, {
+          name: "Unlimited Private Projects",
+          isEnabled: true
+        }, {
+          name: "Dedicated Phone Support",
+          isEnabled: true
+        }, {
+          name: "Free Subdomain",
+          isEnabled: true
+        }, {
+          name: "Monthly Status Reports",
+          isEnabled: false
+        }
+      ]
+
+    },
+    {
+      packageName: "PRO",
+      price: "$49",
+      period: "month",
+      features: [
+        {
+          name: "Unlimited Users",
+          isEnabled: true,
+          isBold: true
+        },
+        {
+          name: "150GB Storage",
+          isEnabled: true
+        }, {
+          name: "Unlimited Public Projects",
+          isEnabled: true
+        }, {
+          name: "Community Access",
+          isEnabled: true
+        }, {
+          name: "Unlimited Private Projects",
+          isEnabled: true
+        }, {
+          name: "Dedicated Phone Support",
+          isEnabled: true
+        }, {
+          name: "Free Subdomain",
+          isEnabled: true,
+          isunlimited:true
+        }, {
+          name: "Monthly Status Reports",
+          isEnabled: true
+        }
+      ]
+
+    }
+  ];
+  return (
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+
+          {pricingData.map((obj, index) => {
+            return <PriceCard key={`C${index}`} Data={obj} />
+          })}
+
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
